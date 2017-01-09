c06ecdab603eb46731d2609af30714ceea42df93
anandhi28 <anandhi.n@thomsonreuters.com>
2017-01-09 16:01:02 +0530

    menubar code changes

--- src/modules/components/menubar/directive/rv-menubar-directive.js
+++ src/modules/components/menubar/directive/rv-menubar-directive.js
@@ -14,22 +14,22 @@
       restrict: 'EA',
       templateUrl: 'modules/components/Menubar/templates/rv-Menubar-template.html',
       controller: 'rvMenubarController',
-      controllerAs: 'vm',
+      controllerAs: 'menuCtrl',
       bindToController: {
         menuItems:'='
       },
-      link: function(_scope, _elements, _attrs,_ctrl){
+      link: function(_scope, _elements, _attrs, _ctrl) {
         console.log(_ctrl.menuItems);
-     }
+      }
     };
   }
   RVMenubarCtrl.$inject = [];
 
-  function RVMenubarCtrl () {
+  function RVMenubarCtrl() {
     var vm = this;
     vm.menuOptions = vm.menuItems;
-    vm.handleClick = function(menuName){
+    vm.handleClick = function(menuName) {
       alert(menuName);
-    };    
+    };
   }
 })(angular);
